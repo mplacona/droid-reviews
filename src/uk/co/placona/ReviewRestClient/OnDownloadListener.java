b1c585747788e337aaa821d856177edd2f2bd77a
Marcos Placona <marcos.placona@gmail.com>
2013-08-17 20:33:23 +0100

    Remove file as it's been renamed

--- src/uk/co/placona/ReviewRestClient/OnDownloadListener.java
+++
@@ -1,8 +0,0 @@
-package uk.co.placona.ReviewRestClient;
-
-import org.json.JSONArray;
-
-public interface OnDownloadListener {
-	public void onDownloadSuccess(JSONArray reviews);
-    public void onDownloadFailure();
-}
