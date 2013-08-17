27dbd0c7e5e87fdcbf88b69a012d80b9db88b6cc
Marcos Placona <marcos.placona@gmail.com>
2013-08-17 20:59:11 +0100

    Remove TextEdit as no longer used

--- src/uk/co/placona/review_sample/MainActivity.java
+++ src/uk/co/placona/review_sample/MainActivity.java
@@ -18,14 +18,12 @@ import uk.co.placona.ReviewRestClient.ReviewRestClient;
 
 public class MainActivity extends Activity implements IDownloadListener {
     private ReviewAdapter adpt;
-	private EditText txt;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        txt = (EditText) this.findViewById(R.id.editText1);
         adpt  = new ReviewAdapter(new ArrayList<Review>(), this);
         ListView lView = (ListView) findViewById(R.id.listView1);
         
@@ -41,20 +39,6 @@ public class MainActivity extends Activity implements IDownloadListener {
         }
         
     }
-    
-    /*
-    private JSONArray fakeJsonObject(){
-    	JSONArray object = null;
-		String s = "[{\"id\":1,\"name\":\"Marcos\",\"text\":\"This is some text\",\"created_at\":\"2013-08-14T08:37:05+01:00\",\"updated_at\":\"2013-08-14T08:37:05+01:00\"},{\"id\":2,\"name\":\"Marcos\",\"text\":\"This is some text\",\"created_at\":\"2013-08-14T08:37:36+01:00\",\"updated_at\":\"2013-08-14T08:37:36+01:00\"},{\"id\":3,\"name\":\"Marcos\",\"text\":\"another review updated\",\"created_at\":\"2013-08-14T08:48:50+01:00\",\"updated_at\":\"2013-08-14T08:48:50+01:00\"}]";
-		try {
-			object = new JSONArray(s);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return object;
-    }*/
     
     private Review convertReview(JSONObject obj) throws JSONException{   	
 		return new Review(obj.getString("name"), obj.getString("text"));    	
@@ -72,7 +56,6 @@ public class MainActivity extends Activity implements IDownloadListener {
 			}
 		}
 		adpt.setItemList(result);
-		txt.setText(reviews.toString());
 	}
 
 	public void onDownloadFailure() {
