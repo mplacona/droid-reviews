398683a556682b1124f03bddcde71aa120f596ea
Marcos Placona <marcos.placona@gmail.com>
2013-11-30 00:36:45 +0000

    Add Genymotion IP address

--- src/uk/co/placona/ReviewRestClient/ReviewRestClientHttp.java
+++ src/uk/co/placona/ReviewRestClient/ReviewRestClientHttp.java
@@ -6,7 +6,8 @@ import com.loopj.android.http.RequestParams;
 
 public class ReviewRestClientHttp {
     private static final int TIMEOUT = 3000;
-	private static final String BASE_URL = "http://10.0.2.2:9393/";
+	//private static final String BASE_URL = "http://10.0.2.2:9393/";
+    private static final String BASE_URL = "http://10.0.3.2:9393/";
 	private static AsyncHttpClient client = new AsyncHttpClient();
 	
 	public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler){
